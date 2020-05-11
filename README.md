fffb2621c518a06569e66bb69afe885e6bc35058
Midgento <CEO.Midgento@users.noreply.github.com>
2020-05-11 19:07:47 +0100

    Update 1.0c The cleanup
    Updating the directory to make it more clear and concise

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ISAD157-A-D-Project
-This is the repository for the collections required to meet the ISAD Analysis &amp; Design Project
+This is the repository for the collections required to meet the ISAD Analysis & Design Project
---
+++ Social Media Database/SocialMediaDatabase/DatabaseForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SOFT562Week18
+{
+    partial class DatabaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewCustomer = new System.Windows.Forms.DataGridView();
+            this.dataGridViewProduct = new System.Windows.Forms.DataGridView();
+            this.comboBoxQueryChoice = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCustomer
+            // 
+            this.dataGridViewCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCustomer.Location = new System.Drawing.Point(15, 12);
+            this.dataGridViewCustomer.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewCustomer.Name = "dataGridViewCustomer";
+            this.dataGridViewCustomer.RowHeadersWidth = 51;
+            this.dataGridViewCustomer.RowTemplate.Height = 24;
+            this.dataGridViewCustomer.Size = new System.Drawing.Size(276, 301);
+            this.dataGridViewCustomer.TabIndex = 0;
+            this.dataGridViewCustomer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCustomer_CellContentClick);
+            // 
+            // dataGridViewProduct
+            // 
+            this.dataGridViewProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProduct.Location = new System.Drawing.Point(310, 12);
+            this.dataGridViewProduct.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewProduct.Name = "dataGridViewProduct";
+            this.dataGridViewProduct.RowHeadersWidth = 51;
+            this.dataGridViewProduct.RowTemplate.Height = 24;
+            this.dataGridViewProduct.Size = new System.Drawing.Size(273, 301);
+            this.dataGridViewProduct.TabIndex = 1;
+            this.dataGridViewProduct.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewProduct_CellContentClick);
+            // 
+            // comboBoxQueryChoice
+            // 
+            this.comboBoxQueryChoice.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxQueryChoice.FormattingEnabled = true;
+            this.comboBoxQueryChoice.Items.AddRange(new object[] {
+            "Users",
+            "Messages"});
+            this.comboBoxQueryChoice.Location = new System.Drawing.Point(15, 328);
+            this.comboBoxQueryChoice.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxQueryChoice.Name = "comboBoxQueryChoice";
+            this.comboBoxQueryChoice.Size = new System.Drawing.Size(569, 29);
+            this.comboBoxQueryChoice.TabIndex = 2;
+            this.comboBoxQueryChoice.SelectedIndexChanged += new System.EventHandler(this.comboBoxQueryChoice_SelectedIndexChanged);
+            // 
+            // DatabaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 717);
+            this.Controls.Add(this.comboBoxQueryChoice);
+            this.Controls.Add(this.dataGridViewProduct);
+            this.Controls.Add(this.dataGridViewCustomer);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "DatabaseForm";
+            this.Text = "Database Form Test";
+            this.Load += new System.EventHandler(this.DatabaseForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCustomer;
+        private System.Windows.Forms.DataGridView dataGridViewProduct;
+        private System.Windows.Forms.ComboBox comboBoxQueryChoice;
+    }
+}
+
